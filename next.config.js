c62668b84f23af11cc623b6ee8ecaea444e5d4f1
bipinemp <bhandaribipin3.39@gmail.com>
2023-10-28 12:32:24 +0545

    changes

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    BLOG_BASE_URL: "https://writz.vercel.app/api/blogs",
-    PROFILE_BASE_URL: "https://writz.vercel.app/api/profile",
-
-    GITHUB_ID: "cc94f119f0ace68340f5",
-    GITHUB_SECRET: "9b9eaf6103f3568104811a455c065a766ca15b1e",
-
-    GOOGLE_ID:
-      "726150958395-ver5coha855mga6cb1284spf3upf3bvo.apps.googleusercontent.com",
-    GOOGLE_SECRET: "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",
-
-    NEXTAUTH_SECRET: "fakjdlfjdklfjaklf#!@#JKL@JK$@@#KL@#K@L@#@",
-
-    // NEXTAUTH_URL: "http://localhost:3000",
-    NEXTAUTH_URL: "https://writz.vercel.app",
-
-    MONGO_URL:
-      "mongodb+srv://bipinbhandari:gaindakot@nodejstutorial.bv7ep8i.mongodb.net/blogwebsite?retryWrites=true&w=majority",
-  },
   images: {
     remotePatterns: [
       {
@@ -28,8 +9,5 @@ const nextConfig = {
     ],
   },
 };
-
-// GOOGLE_ID="726150958395-ver5coha855mga6cb1284spf3upf3bvo.apps.googleusercontent.com"
-// GOOGLE_SECRET="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"
 
 module.exports = nextConfig;
